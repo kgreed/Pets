3b60f1524e6aacda3490c23b4e12a40bca0b15e4
Kirsten Greed <kirsteng@jobtalk.com.au>
2022-04-24 12:17:30 +1000

    Working

--- Pets.Module/BusinessObjects/Kitten.cs
+++ Pets.Module/BusinessObjects/Kitten.cs
@@ -6,8 +6,8 @@ namespace Pets.Module.BusinessObjects
     public class Kitten : BabyPet
     {
         // comment out the following to solve the issue
-        public int ParentPetId { get; set; }
-        [ForeignKey("ParentPetId")]
+        //public int ParentPetId { get; set; }
+        //[ForeignKey("ParentPetId")]
         // end comment
         public virtual Cat Parent { get; set; }
     }
--- Pets.Module/BusinessObjects/Puppy.cs
+++ Pets.Module/BusinessObjects/Puppy.cs
@@ -6,8 +6,8 @@ namespace Pets.Module.BusinessObjects
     public class Puppy : BabyPet
     {
         // comment out the following to solve the problem
-        public int ParentPetId { get; set; }
-        [ForeignKey("ParentPetId")]
+        //public int ParentPetId { get; set; }
+        //[ForeignKey("ParentPetId")]
         // end  comment
         public virtual Dog Parent { get; set; }
 
